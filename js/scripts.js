b7a4a5c0f216e9d61e49d90072c76eb796550ea4
tamirescalixto <tamires.americo.calixto@gmail.com>
2022-09-30 15:22:50 -0300

    DesafioRocketNews

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-function send(texto){
-    alert(texto);
+function send(){
+    let email = document.getElementById("email").value;
+
+    if (email) {
+        alert(`Email ${email} cadastrado com sucesso!`);
+    } else {
+        alert("Por favor, preencha o campo email!");
+    }
 }
